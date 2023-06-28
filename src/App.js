5786eceda071d39672d7b338a9b89028b56015e6
dattatreyav30 <dattatreya361@gmail.com>
2023-06-28 22:09:47 +0530

    hard coded cart items

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import CartItems from "./components/CartItems/CartItems";
+
+const App = () => {
+  return (
+    <Fragment>
+      <CartItems />
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ src/components/CartItems/CartItems.js
@@ -0,0 +1,43 @@
+import { Fragment } from "react";
+
+import './CartItems.css'
+const productsArr = [
+  {
+    title: "Colors",
+    price: 100,
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+  },
+  {
+    title: "Black and white Colors",
+    price: 50,
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+  },
+  {
+    title: "Yellow and Black Colors",
+    price: 70,
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+  },
+  {
+    title: "Blue Color",
+    price: 100,
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+  },
+];
+
+const CartItems = () => {
+  return (
+    <Fragment>
+      {productsArr.map((item) => {
+        return (
+          <div className="items">
+            <h2>{item.title}</h2>
+            <img alt="item" src={item.imageUrl} />
+            <h4>{item.price}$</h4>
+          </div>
+        );
+      })}
+    </Fragment>
+  );
+};
+
+export default CartItems;
