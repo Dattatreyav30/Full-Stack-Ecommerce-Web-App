ab8647812fa2b94c5d59d0f4bba35853165b9ba3
dattatreyav30 <dattatreya361@gmail.com>
2023-07-02 10:57:46 +0530

    loading the application

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -6,11 +6,15 @@ const ProductItems = () => {
   const cartCtx = useContext(CartContext);
 
   const [items, setItems] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
+      setisLoading(true);
       const response = await fetch("http://localhost:5000/");
       const data = await response.json();
       setItems(data.products);
+      setisLoading(false);
     };
     fetchData();
   }, []);
@@ -20,26 +24,30 @@ const ProductItems = () => {
   };
 
   return (
-    <div className="items">
-      {items.map((item) => {
-        return (
-          <div key={item.id}>
-            <h2 className="item-title">{item.title}</h2>
-            <img className="item-image" alt="item" src={item.imageUrl} />
-            <div className="item-details">
-              <div className="price-add-container">
-                <h3 className="item-price">Price - {item.price}$</h3>
-                <Button
-                  onClick={() => addItemHandler(item)}
-                  className="add-button"
-                  value="Add to Cart"
-                />
+    <>
+      <div className="items">
+        {!isLoading &&
+          items.map((item) => {
+            return (
+              <div key={item.id}>
+                <h2 className="item-title">{item.title}</h2>
+                <img className="item-image" alt="item" src={item.imageUrl} />
+                <div className="item-details">
+                  <div className="price-add-container">
+                    <h3 className="item-price">Price - {item.price}$</h3>
+                    <Button
+                      onClick={() => addItemHandler(item)}
+                      className="add-button"
+                      value="Add to Cart"
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+            );
+          })}
+      </div>
+      {isLoading && <h1 className="loading-text">Loading.....</h1>}
+    </>
   );
 };
 
--- server/controllers/ItemsController.js
+++ server/controllers/ItemsController.js
@@ -1,7 +1,6 @@
 const Items = require("../Models/itemsModel");
 
 exports.getProducts = async (req, res, next) => {
-  console.log("executed");
   const products = await Items.findAll({
     attributes: { exclude: ['createdAt', 'updatedAt'] }
   });
